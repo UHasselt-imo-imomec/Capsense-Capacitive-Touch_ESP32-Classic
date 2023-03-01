7b8453d758679cdf161a5ee635574c4aa0450678
T. Vandenryt <24638205+vanThijs@users.noreply.github.com>
2023-03-01 10:07:33 +0100

    Added variable for the touch pin, and improved touch pin legend.

--- ESP32_Touch/ESP32_Touch.ino
+++ ESP32_Touch/ESP32_Touch.ino
@@ -11,18 +11,19 @@ Touch Sensor Pin Layout
 T0 = GPIO4
 T1 = GPIO0
 T2 = GPIO2
-T3 = GPIO15
-T4 = GPIO13
-T5 = GPIO12    --> Currently in use
-T6 = GPIO14
-T7 = GPIO27
-T8 = GPIO33
-T9 = GPIO32
+T3 = GPIO15 (MTDO)
+T4 = GPIO13 (MTCK)
+T5 = GPIO12 (MTDI)   --> Currently in use
+T6 = GPIO14 (MTMS)
+T7 = GPIO27 
+T8 = GPIO33 (32K_XN)
+T9 = GPIO32 (32K_XP)
+
 
 
 Touch settings:
 touch_pad_set_voltage(TOUCH_HVOLT_2V4,TOUCH_LVOLT_0V8,TOUCH_HVOLT_ATTEN_1V5);    //0,3,3   --> Check att voltages, now 0 att.
-touch_pad_set_cnt_mode(TOUCH_PAD_NUM5,TOUCH_PAD_SLOPE_7,TOUCH_PAD_TIE_OPT_LOW)
+touch_pad_set_cnt_mode(TOUCHPIN,TOUCH_PAD_SLOPE_7,TOUCH_PAD_TIE_OPT_LOW)
 
 DAC pin: DAC_1 == GPIO_25		(8Bit real DAC, No PWM)
 
@@ -31,12 +32,14 @@ Additional information: http://esp-idf.readthedocs.io/en/latest/api-reference/pe
 
 																								Thijs Vandenryt 2018
 */
-#include "driver/DAC.h" 	 //Required to use the DAC outputs
-#include <driver/touch_pad.h>   //Required for touch functionality
+#include "driver/DAC.h" 	 		//Required to use the DAC outputs
+#include <driver/touch_pad.h>   	//Required for touch functionality
 
-#define MAX_CAP_VAL 1500	//Maximum recorded value for the given belt (can be collected in a calibration routine: e.g. 3x max depth inhalation
-#define MIN_CAP_VAL 1200	//Lowest value recorded for the given belt/patient
-#define DelayTimeMS 90		//Time between new capcitive datapoints
+#define MAX_CAP_VAL 1500			//Maximum recorded value for the given belt (can be collected in a calibration routine: e.g. 3x max depth inhalation
+#define MIN_CAP_VAL 1200			//Lowest value recorded for the given belt/patient
+#define DelayTimeMS 90				//Time between new capcitive datapoints
+#define TOUCHPIN TOUCH_PAD_NUM5		//Num5==GPIO12
+
 
 void setup()
 {
@@ -48,7 +51,7 @@ void setup()
 		Serial.println("!!!!Init ERROR");
 	}
 
-	touch_pad_config(TOUCH_PAD_NUM5, 0);			//Num5==GPIO12
+	touch_pad_config(TOUCHPIN, 0);			
 
 	if (touch_pad_filter_start(10)) {				//Setup filter setup (10ms filter duration)
 		Serial.println("!!!!Filter Start ERROR");
@@ -65,7 +68,7 @@ void loop()
 
 	PrintThresholdSettings();
 
-	if (touch_pad_set_cnt_mode(TOUCH_PAD_NUM5, TOUCH_PAD_SLOPE_7, TOUCH_PAD_TIE_OPT_LOW)) { //slope_1: Slowest ,7 fastest, 4 Default
+	if (touch_pad_set_cnt_mode(TOUCHPIN, TOUCH_PAD_SLOPE_7, TOUCH_PAD_TIE_OPT_LOW)) { //slope_1: Slowest ,7 fastest, 4 Default
 		Serial.println("!!!!Slope setting ERROR");
 	}
 
@@ -85,8 +88,8 @@ void loop()
 	//Superloop
 	while (1) {
 		if (TempTime+DelayTimeMS <= (millis())) {     //If DelayTimeMS has elapsed, new datapoint is read
-			if (touch_pad_read_filtered(TOUCH_PAD_NUM5, &ReadVal[0])) {	//Raw data is read 
-																		//if(touch_pad_read_filtered(TOUCH_PAD_NUM5,&ReadVal){		//Filtered data is read--> Needs initialisation with "touch_pad_filter_start(Filter_time_in_ms)"
+			if (touch_pad_read_filtered(TOUCHPIN, &ReadVal[0])) {	//Raw data is read 
+																		//if(touch_pad_read_filtered(TOUCHPIN,&ReadVal){		//Filtered data is read--> Needs initialisation with "touch_pad_filter_start(Filter_time_in_ms)"
 				Serial.println("Read filtered value error");
 			}
 			else if (ReadVal[0] == ReadVal[1]){		//No update is neccessary
@@ -147,14 +150,14 @@ void PrintVoltageSettings(void)		//Prints the excitation voltage settings
 void PrintThresholdSettings(void) {
 	uint16_t retval = 100;
 
-	Serial.print("Threshold: Status "); Serial.print(touch_pad_get_thresh(TOUCH_PAD_NUM5, &retval));
+	Serial.print("Threshold: Status "); Serial.print(touch_pad_get_thresh(TOUCHPIN, &retval));
 	Serial.print(", Threshold "); 	Serial.println(retval);
 }
 
 void PrintSlopeSettings(void) {
 	uint16_t retval = 100, retval2 = 100;
 
-	Serial.print("Slope: Status "); Serial.print(touch_pad_get_cnt_mode(TOUCH_PAD_NUM5, (touch_cnt_slope_t*)&retval, (touch_tie_opt_t*)&retval2));
+	Serial.print("Slope: Status "); Serial.print(touch_pad_get_cnt_mode(TOUCHPIN, (touch_cnt_slope_t*)&retval, (touch_tie_opt_t*)&retval2));
 	Serial.print(", Slope "); Serial.print(retval);
 	Serial.print(", Init Voltage "); Serial.println(retval2);
 }
